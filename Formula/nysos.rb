0e83f87eb284804d37fdef2cdc588f0c91e3e713
Marshall Cody McCain (McCodeman) <cody@aviznetworks.com>
2026-09-22 11:51:45 -0500

    chore: package nysos 0.1.2

--- Formula/nysos.rb
+++ Formula/nysos.rb
@@ -1,8 +1,8 @@
 class Nysos < Formula
   desc "Multi-pane scripted and interactive terminal demonstrations"
   homepage "https://github.com/McCodeman/nysos"
-  url "https://github.com/McCodeman/nysos/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "2c1a76c5779bf932c69aa9a3cdd27910deaf036a7f7be0b719d1778d1ee16e6c"
+  url "https://github.com/McCodeman/nysos/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "886bb312f54bcfaad322e8cab145ff345d9ca3ae4f0edf9c6c2c546c50618ad1"
   license "Apache-2.0"
   head "https://github.com/McCodeman/nysos.git", branch: "main"
 
@@ -10,9 +10,9 @@ class Nysos < Formula
 
   def install
     if build.stable?
-      ENV["NYSOS_GIT_COMMIT"] = "3919f34ddd4acecd6f2b1ab94023aca9c7de7c72"
-      ENV["NYSOS_GIT_TAG"] = "v0.1.1"
-      ENV["NYSOS_GIT_DESCRIBE"] = "v0.1.1"
+      ENV["NYSOS_GIT_COMMIT"] = "d25783b0f2de0adc2efb17aebfa2c81f149de471"
+      ENV["NYSOS_GIT_TAG"] = "v0.1.2"
+      ENV["NYSOS_GIT_DESCRIBE"] = "v0.1.2"
     end
     system "cargo", "install", *std_cargo_args
     man1.install "docs/man/nysos.1"
@@ -24,7 +24,7 @@ class Nysos < Formula
     assert_match version.to_s, shell_output("#{bin}/nysos -V")
     assert_match "Git commit:", shell_output("#{bin}/nysos --version-full")
     assert_match "Valid: 2 panes, 0 queue items", shell_output("#{bin}/nysos --check")
-    assert_match "Valid: 2 panes, 6 queue items", shell_output("#{bin}/nysos --demo --check")
+    assert_match "Valid: 3 panes, 6 queue items", shell_output("#{bin}/nysos --demo --check")
     system bin/"nysos", "--init", "demo.toml"
     assert_match "Valid:", shell_output("#{bin}/nysos --config demo.toml --check")
   end
